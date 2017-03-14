cfbb5bf74a826fd552d81baf2dcb12fa20112eaa
wangliang <wangliang@news.cn>
2017-03-14 14:18:49 +0800

    first commit

---
+++ src/com/liang/algo/OverPadding.java
@@ -0,0 +1,140 @@
+package com.liang.algo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 区间重合判断
+ * @author Lyon
+ * @date 2016/05/03
+ */
+class Node{
+	private int x;
+	private int y;
+	public Node(int x,int y){
+		this.x=x;
+		this.y=y;
+	}
+	public Node(){
+		
+	}
+	public void setX(int x){
+		this.x=x;
+	}
+	public void setY(int y){
+		this.y=y;
+	}
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+	@Override
+	public String toString(){
+		return "x="+x+", y="+y;
+	}
+}
+public class OverPadding {
+	/**
+	 * 合并排序后的区间，
+	 * @param nodes
+	 * @return
+	 */
+	public static List<Node> mergeInterval(List<Node> nodes){
+		List<Node>sortedNode=new ArrayList<>();
+		if(nodes.size()>1){
+			Node cur=nodes.get(0);
+			int i=1;
+			while(i<nodes.size()){
+				if(nodes.get(i).getX()==cur.getY()){
+					cur.setY(nodes.get(i).getY());	
+				}
+				else{
+					sortedNode.add(cur);
+					cur=nodes.get(i);
+				}	
+				i++;
+			}
+			sortedNode.add(cur);  //添加最后一个区间
+		}
+		return sortedNode;
+	}
+	//对区间进行排序
+	public static void sortInterval(List<Node> nodes2){
+		Node[]nodes=(Node[]) nodes2.toArray(new Node[nodes2.size()]);
+		Arrays.sort(nodes,new Comparator<Node>(){
+
+			@Override
+			public int compare(Node o1, Node o2) {
+				// TODO Auto-generated method stub
+				return o1.getX()<o2.getX()?-1:(o1.getX()==o2.getX()?0:1);
+			}
+			
+		});
+		nodes2.clear(); //清空原先数据，重新依次插入
+		for(Node node:nodes)
+			nodes2.add(node);
+	}
+	/**
+	 * 在list中查询指定区间node
+	 * @param list
+	 * @param node
+	 * @return true if find the node
+	 */
+	public static boolean search(List<Node>list,Node node){
+		for(Node a:list){
+			if(a.getX()<=node.getX()&&a.getY()>=node.getY())
+				return true;
+		}
+		return false;
+	}
+	public static void PrintInterval(List<Node>list){
+		for(Node node:list){
+			System.out.println(node);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader in=null;
+		List<Node>nodes=new ArrayList<Node>();
+		try{
+			in=new BufferedReader(new FileReader(new File("interval.txt")));
+			String line;
+			while((line=in.readLine())!=null){
+				String[]xy=line.split(" ");
+				int x=Integer.valueOf(xy[0]);
+				int y=Integer.valueOf(xy[1]);
+				Node node=new Node(x,y);
+				nodes.add(node);
+			}
+			PrintInterval(nodes);
+			System.out.println("---sorted---");
+			sortInterval(nodes);
+			PrintInterval(nodes);
+			
+			nodes=mergeInterval(nodes);
+			System.out.println("---merged---");
+			PrintInterval(nodes);
+			
+			Node node=new Node(1,3);
+			System.out.println("Search interval[ "+node.getX()+" "+node.getY()+"]");
+			System.out.println(search(nodes,node));
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				in.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/com/liang/algo/PrimeRing.java
@@ -0,0 +1,76 @@
+package com.liang.algo;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * 由于n<20,所以可以预处理前40个自然数中的素数，然后深度搜索某个位置的未被访问过的数字和它相邻的数字之和是否为素数
+ * 退出搜索的条件是最后一个位置的数字arr[n]+1为素数，每次搜索完成后，要回溯
+ */
+
+
+public class PrimeRing {
+	private static int Primes[]={2,3,5,7,11,13,17,19,23,29,31,37};
+	public static void Print(int[]arr){
+		for(int a:arr)
+			System.out.print(a+" ");
+		System.out.println();
+	}
+	/**
+	 * Judge n is a prime number
+	 * @param n
+	 * @return
+	 */
+	public static boolean isPrime(int n){
+		if(n<=1)
+			return false;
+		for(int j=2;j<=Math.sqrt(n);j++){
+			if(n%j==0)
+				return false;
+		}
+		return true;
+	}
+	public static boolean is_prime(int a){
+		for(int i=0;i<Primes.length;i++)
+			if(a==Primes[i])
+				return true;
+		return false;	
+	}
+	/**
+	 * Given n, output circle clockwisely and anticlockwisely
+	 * @param start 
+	 * @param end
+	 */
+	public static void dfs(int cur,int []arr,boolean[]visited){
+		if(cur==arr.length-1){
+			if(is_prime(arr[cur]+arr[0]))  //search end
+				Print(arr);
+			return;
+		}
+		for(int i=2;i<=arr.length;i++){
+	        if (!visited[i-1]&& is_prime(i+arr[cur])){
+	            arr[cur+1] = i;
+	            visited[i-1] = true;
+	            dfs(cur+1,arr,visited);
+	            visited[i-1] = false;  //reset for trace back
+	        }
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		int n=in.nextInt();
+		int arr[]=new int[n];
+		boolean []visited=new boolean[n];
+		arr[0]=1;
+		visited[0]=true;
+		for(int i=1;i<n;i++){
+			arr[i]=i+1;
+			visited[i]=false;
+		}
+		dfs(0,arr,visited);
+	}
+}
+
